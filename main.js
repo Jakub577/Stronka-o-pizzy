e4803e3721ae854198a5923b4f557977b9b8e58f
Jakub577 <71120205+Jakub577@users.noreply.github.com>
2021-04-30 14:44:33 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+//function sumowaniewyarz() {
+//    let input1 = document.getElementById('inpucik1').value
+//    let input2 = document.getElementById('inpucik2').value
+//    let wysiwietl = input1 + input2
+//    if (input1 + input2 === ""){
+//        alert("oba divy są puste ")
+//    }
+//    console.log(wysiwietl)
+//}
+//
+//function sumowaniewyarz1() {//
+//  const inputs = document.querySelectorAll('[type="text"]')
+//  let input = inputs[0].value
+//  input = parseInt.value
+//  if(input1 !== NaN){
+
+//  }
+//}
+//<img class="img1" src="Pizza-Margherita-leggera-ricettasprint-2.jpg" alt="">
